d79d1d7fd49b8255c6546237b6da647b268c513d
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-03-06 17:52:11 +0000

    adds ICBM

---
+++ src/main/java/ICBM.java
@@ -0,0 +1,7 @@
+public class ICBM extends Army{
+
+    public ICBM(String name, int healthValue, int attackValue) {
+        super(name, healthValue, attackValue);
+    }
+
+}
---
+++ src/test/java/ICBMTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ICBMTest {
+
+    private ICBM icbm;
+    private BigMonkey bigMonkey;
+
+    @Before
+    public void setUp() {
+        icbm = new ICBM("Miss Isle", 3000, 100);
+        bigMonkey = new BigMonkey("King Kong", 20, 7);
+    }
+
+    @Test
+    public void testCanGetName(){
+        assertEquals("Miss Isle", icbm.getName());
+    }
+
+    @Test
+    public void testCanGetHealthValue() {
+        assertEquals(3000, icbm.getHealthValue());
+    }
+
+    @Test
+    public void testCanGetAttackValue() {
+        assertEquals(100, icbm.getAttackValue());
+    }
+
+    @Test
+    public void testCanAttackMonster() {
+        assertEquals(100, icbm.attackMonster());
+    }
+
+    @Test
+    public void testHealthValueIsReducedAfterMonsterAttach(){
+        assertEquals(2993, icbm.incomingAttack(bigMonkey));
+    }
+}
